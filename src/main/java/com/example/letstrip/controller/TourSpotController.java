06abb76427cd7acb70defc5f3bf04ec3e642c813
jeongyeonKimm <wjddus9132@gmail.com>
2023-05-08 19:08:48 +0900

    refactor: 경로 반환 리턴 타입 변경

--- src/main/java/com/example/letstrip/controller/TourSpotController.java
+++ src/main/java/com/example/letstrip/controller/TourSpotController.java
@@ -22,47 +22,83 @@ public class TourSpotController {
     private final TourSpotService tourSpotService;
 
     @GetMapping("/course")
-    public ResponseEntity<?> getRoute(@RequestBody List<RouteDto> routeDtoList) {
-        try {
-            List<List<TourSpotDto>> responseDto = new ArrayList<>();
-            for (int i = 0; i < routeDtoList.size(); i++) {
-                List<TourSpot> spots = tourSpotService.getTourSpot(routeDtoList.get(i).getNames());
+    public List<List<TourSpotDto>> getRoute(@RequestBody List<RouteDto> routeDtoList) {
+//        try {
+//            List<List<TourSpotDto>> responseDto = new ArrayList<>();
+//            for (int i = 0; i < routeDtoList.size(); i++) {
+//                List<TourSpot> spots = tourSpotService.getTourSpot(routeDtoList.get(i).getNames());
+//
+//                List<TourSpotDto> dayCourse = new ArrayList<>();
+//                int idx = 0;
+//                for (TourSpot spot : spots) {
+//                    TourSpotDto dto = TourSpotDto.builder()
+//                            .id(spot.getId())
+//                            .name(spot.getName())
+//                            .imgUrl(spot.getImgUrl())
+//                            .address(spot.getAddress())
+//                            .localAddress(spot.getLocalAddress())
+//                            .phoneNumber(spot.getPhoneNumber())
+//                            .sun(spot.getSun())
+//                            .mon(spot.getMon())
+//                            .tue(spot.getTue())
+//                            .wed(spot.getWed())
+//                            .thu(spot.getThu())
+//                            .fri(spot.getFri())
+//                            .sat(spot.getSat())
+//                            .overview(spot.getOverview())
+//                            .latitude(spot.getLatitude())
+//                            .longitude(spot.getLongitude())
+//                            .arrive_time(routeDtoList.get(i).getArrive_times().get(idx) != null ? routeDtoList.get(i).getArrive_times().get(idx).substring(0, 19) : null)
+//                            .depart_time(routeDtoList.get(i).getDepart_times().get(idx).substring(0, 19))
+//                            .move_time(routeDtoList.get(i).getMove_times().get(idx))
+//                            .keywordImgUrl(spot.getKeywordImgUrl())
+//                            .build();
+//                    dayCourse.add(dto);
+//                    idx++;
+//                }
+//                responseDto.add(dayCourse);
+//            }
+//
+//            return responseDto;
+//        } catch (Exception e) {
+//            ResponseDto responseDto = ResponseDto.builder().error(e.getMessage()).build();
+//            return responseDto;
+//        }
+        List<List<TourSpotDto>> responseDto = new ArrayList<>();
+        for (int i = 0; i < routeDtoList.size(); i++) {
+            List<TourSpot> spots = tourSpotService.getTourSpot(routeDtoList.get(i).getNames());
 
-                List<TourSpotDto> dayCourse = new ArrayList<>();
-                int idx = 0;
-                for (TourSpot spot : spots) {
-                    TourSpotDto dto = TourSpotDto.builder()
-                            .id(spot.getId())
-                            .name(spot.getName())
-                            .imgUrl(spot.getImgUrl())
-                            .address(spot.getAddress())
-                            .localAddress(spot.getLocalAddress())
-                            .phoneNumber(spot.getPhoneNumber())
-                            .sun(spot.getSun())
-                            .mon(spot.getMon())
-                            .tue(spot.getTue())
-                            .wed(spot.getWed())
-                            .thu(spot.getThu())
-                            .fri(spot.getFri())
-                            .sat(spot.getSat())
-                            .overview(spot.getOverview())
-                            .latitude(spot.getLatitude())
-                            .longitude(spot.getLongitude())
-                            .arrive_time(routeDtoList.get(i).getArrive_times().get(idx) != null ? routeDtoList.get(i).getArrive_times().get(idx).substring(0, 19) : null)
-                            .depart_time(routeDtoList.get(i).getDepart_times().get(idx).substring(0, 19))
-                            .move_time(routeDtoList.get(i).getMove_times().get(idx))
-                            .keywordImgUrl(spot.getKeywordImgUrl())
-                            .build();
-                    dayCourse.add(dto);
-                    idx++;
-                }
-                responseDto.add(dayCourse);
+            List<TourSpotDto> dayCourse = new ArrayList<>();
+            int idx = 0;
+            for (TourSpot spot : spots) {
+                TourSpotDto dto = TourSpotDto.builder()
+                        .id(spot.getId())
+                        .name(spot.getName())
+                        .imgUrl(spot.getImgUrl())
+                        .address(spot.getAddress())
+                        .localAddress(spot.getLocalAddress())
+                        .phoneNumber(spot.getPhoneNumber())
+                        .sun(spot.getSun())
+                        .mon(spot.getMon())
+                        .tue(spot.getTue())
+                        .wed(spot.getWed())
+                        .thu(spot.getThu())
+                        .fri(spot.getFri())
+                        .sat(spot.getSat())
+                        .overview(spot.getOverview())
+                        .latitude(spot.getLatitude())
+                        .longitude(spot.getLongitude())
+                        .arrive_time(routeDtoList.get(i).getArrive_times().get(idx) != null ? routeDtoList.get(i).getArrive_times().get(idx).substring(0, 19) : null)
+                        .depart_time(routeDtoList.get(i).getDepart_times().get(idx).substring(0, 19))
+                        .move_time(routeDtoList.get(i).getMove_times().get(idx))
+                        .keywordImgUrl(spot.getKeywordImgUrl())
+                        .build();
+                dayCourse.add(dto);
+                idx++;
             }
-
-            return ResponseEntity.ok().body(responseDto);
-        } catch (Exception e) {
-            ResponseDto responseDto = ResponseDto.builder().error(e.getMessage()).build();
-            return ResponseEntity.badRequest().body(responseDto);
+            responseDto.add(dayCourse);
         }
+
+        return responseDto;
     }
 }
